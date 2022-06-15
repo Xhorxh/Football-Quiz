bfe4d17f03eb870dde8e8b10e6fe756b114b541b
George <andrea.xhorxhi@gmail.com>
2022-06-15 11:04:17 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function startQuiz() {
+
+}
+
+function displayQuestion() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function startTimer() {
+
+}
+
+function answerIsCorrect() {
+
+}
+
+function answerIsWrong() {
+
+}
+
+function gameOver() {
+
+}
