325020b431447cfcb05c176b03a2af31a43b31ec
George <andrea.xhorxhi@gmail.com>
2022-06-15 11:16:50 +0000

    Add questions list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,94 @@
+// Quiz questions
+let questions = [{
+    "question": "In which World Cup did Diego Maradona score his infamous 'Hand of God' goal?",
+    "optionA": "Spain 1982",
+    "optionB": "Italy 1990",
+    "optionC": "Mexico 1986",
+    "optionD": "Argentina 1978",
+    "correct": "C"
+  },
+  {
+    "question": "In which season was the European Cup rebranded as the Champions League? ",
+    "optionA": "96-97",
+    "optionB": "91-92",
+    "optionC": "93-94",
+    "optionD": "92-93",
+    "correct": "D"
+  },
+  {
+    "question": "FC Cologne have which animal on their club crest? ",
+    "optionA": "Two Lions",
+    "optionB": "A Fox",
+    "optionC": "A Goat",
+    "optionD": "A Bird",
+    "correct": "C"
+  },
+  {
+    "question": "Where did Milan sign Weah from?",
+    "optionA": "PSG",
+    "optionB": "Monaco",
+    "optionC": "Marseille",
+    "optionD": "Bordeaux",
+    "correct": "A"
+  },
+  {
+    "question": "Which Dutch player became known as the Non-Flying Dutchman?",
+    "optionA": "Robin van Persie",
+    "optionB": "Dennis Bergkamp",
+    "optionC": "Justin Kluivert",
+    "optionD": "Arjen Robben",
+    "correct": "B"
+  },
+  {
+    "question": "Who was the first sponsor of the Premier League?",
+    "optionA": "EA Sports",
+    "optionB": "Barclaycard",
+    "optionC": "Budweiser",
+    "optionD": "Carling",
+    "correct": "D"
+  },
+  {
+    "question": "In which year Ronaldo retired before changing his decision?",
+    "optionA": "1997",
+    "optionB": "1996",
+    "optionC": "1994",
+    "optionD": "1998",
+    "correct": "B"
+  },
+  {
+    "question": "Who won Player of the Tournament in Football Euro Cup 2020?",
+    "optionA": "Pedri",
+    "optionB": "Jorginho",
+    "optionC": "G.Donnarumma",
+    "optionD": "R.Sterling",
+    "correct": "C"
+  },
+  {
+    "question": "Which player holds the record for most Champions League winners' medals?",
+    "optionA": "Cristiano Ronaldo",
+    "optionB": "Francisco Gento",
+    "optionC": "Clarence Seedorf",
+    "optionD": "Paolo Maldini",
+    "correct": "B"
+  },
+ {
+    "question": "In 1990, which team failed to score even one goal in the World Cup final?",
+    "optionA": "Argentina",
+    "optionB": "Egypt",
+    "optionC": "Costa Rica",
+    "optionD": "South Korea",
+    "correct": "A"
+  },
+  {
+    "question": "Who was the best player ever in Partizani Tirana's history?",
+    "optionA": "Igli Tare",
+    "optionB": "Panajot Pano",
+    "optionC": "Refik Resmja",
+    "optionD": "Elton Nikolla",
+    "correct": "D"
+  }
+];
+
 function startQuiz() {
 
 }
